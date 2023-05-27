eddce3f8ed9827d1daf2fef45c1156d5c5dbdc01
paulabian <paulabianciotto@hotmail.com>
2023-05-27 19:12:29 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,8 @@
+let base = 5;
+let altura = 3;
+let area = base * altura;
+console.log (area);
+base = 12.5;
+altura = 5.3;
+area = base * altura;
+console.log (area);
---
+++ practico0/promedio3.js
@@ -0,0 +1,17 @@
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+let promedio = (num1 + num2 + num3) / 3;
+console.log (promedio);
+
+num1 = -2;
+num2 = -3;
+num3 = -8;
+promedio = (num1 + num2 + num3) / 3;
+console.log (promedio);
+
+num1 = -1.2;
+num2 = 5.3;
+num3 = -3.3;
+promedio = (num1 + num2 + num3) / 3;
+console.log (promedio);
